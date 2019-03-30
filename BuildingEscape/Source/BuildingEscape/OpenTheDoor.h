bf782d6416a6a601236107d4f1ddb4dbf120e6be
webbuildermn <46059943+webbuildermn@users.noreply.github.com>
2019-03-30 18:30:36 -0500

    After crash rebuilt old code, the OpenTheDoor. It goes now. It might not go after I refactor code.

---
+++ BuildingEscape/Source/BuildingEscape/OpenTheDoor.h
@@ -0,0 +1,39 @@
+// Copyright Gerald Ryan 2019
+
+#pragma once
+
+#include "Engine/TriggerVolume.h"
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "OpenTheDoor.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class BUILDINGESCAPE_API UOpenTheDoor : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UOpenTheDoor();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+private: 
+	UPROPERTY(VisibleAnywhere)
+	float OpenAngle = 60.0f;
+	
+	UPROPERTY(EditAnywhere)
+	ATriggerVolume* PressurePlate;
+
+	UPROPERTY(EditAnywhere)
+	AActor* ActorThatOpens;
+
+};
