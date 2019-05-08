e70975531ab5a68e84ef20bc61b7e5dae8404e0c
webbuildermn <46059943+webbuildermn@users.noreply.github.com>
2019-05-07 20:57:19 -0500

    Added a grab and release method (right mouse click or left shift click) to begin to make a method to grab an object (chair) to escape the room. Right now these things only log grab status or release status but they're beginning to take shape.

--- BuildingEscape/Source/BuildingEscape/Grabber.h
+++ BuildingEscape/Source/BuildingEscape/Grabber.h
@@ -4,6 +4,8 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "PhysicsEngine/PhysicsHandleComponent.h"
+#include "Components/InputComponent.h"
 #include "Grabber.generated.h"
 
 
@@ -26,5 +28,27 @@ public:
 
 private:
 	float Reach = 100.f;
+
+	UPhysicsHandleComponent* PhysicsHandle = nullptr;
+
+	// This is a UInputComponent type eponymously named, pointed to nothing, but it will be referenced later, in the implementation file. 
+	// It is still not perfectly clear to me why there is a need for a header file and an implementation file. 
+	// It is a variable still but it is a pointer variable, right? Pointers are still variables, just not copies? They look the same on the surface
+	// But behave differently underneath? But I came here to see what type it was, and what else was declared with it. We are going to use this variable
+	// to run methods, so I should know something about this type, I think.
+	// I don't see how BindAction method is linked to this class or type. 
+	// BindAction is a functino of uinputcomponent but I only see that fact when I'm in hte UIC docs not the bindaction docs.	 f
+
+
+
+	UInputComponent* InputComponent = nullptr;
+
+	// Ray Cast and Grab what's in reach
+	void Grab();
+
+	// Called when grab is released
+	void Release();
+
+
 	
 };
